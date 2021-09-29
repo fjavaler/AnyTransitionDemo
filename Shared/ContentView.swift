0c2769e4006e71871d2b6b7ba035b2e8a769aa53
Frederick Javalera <frederickjavalera@gmail.com>
2021-09-28 20:20:29 -0600

    - Finished

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,6 +7,38 @@
 
 import SwiftUI
 
+struct RotateViewModifier: ViewModifier {
+  
+  let rotation: Double
+  
+  func body(content: Content) -> some View {
+    content
+      .rotationEffect(Angle(degrees: rotation))
+      .offset(
+        x:
+          rotation != 0 ? UIScreen.main.bounds.width : 0,
+        y:
+          rotation != 0 ? UIScreen.main.bounds.height : 0)
+  }
+}
+
+extension AnyTransition {
+  
+  static var rotating: AnyTransition {
+    return AnyTransition.modifier(active: RotateViewModifier(rotation: 180), identity: RotateViewModifier(rotation: 0))
+  }
+  
+  static func rotating(rotation: Double) -> AnyTransition {
+    return AnyTransition.modifier(active: RotateViewModifier(rotation: rotation), identity: RotateViewModifier(rotation: 0))
+  }
+  
+  static var rotateOn: AnyTransition {
+    return AnyTransition.asymmetric(
+      insertion: .rotating,
+      removal: .move(edge: .leading))
+  }
+}
+
 struct ContentView: View {
   
   @State private var showRectangle: Bool = false
@@ -18,6 +50,10 @@ struct ContentView: View {
       if showRectangle {
         RoundedRectangle(cornerRadius: 25)
           .frame(width: 250, height: 250)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+        // Uncomment desired transition
+        //.transition(.move(edge: .leading))
+          .transition(.rotateOn)
       }
       
       Spacer()
